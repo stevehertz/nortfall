95d1d3c0209cff6adad2e1134b8ac19c71b0778e
unknown <info@schedulize.co.ke>
2023-01-05 12:31:45 +0300

    Mega Menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo D3.84.0">
     <meta name="theme-color" content="#003366">
-    <title>Nortfall Systems</title>
+    <title>{{ config('app.name') }}</title>
     <!-- Favicons -->
     <link href="{{ asset('storage/logo/apple-touch-icon.png') }}" rel="apple-touch-icon" sizes="180x180" />
     <link href="{{ asset('storage/logo/favicon-32x32.png') }}" rel="icon" sizes="32x32" type="image/png" />
@@ -18,55 +18,15 @@
     <link href="{{ asset('fonts/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('fonts/Ionicons/css/ionicons.min.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <link rel="stylesheet" href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}">
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/main.css') }}" rel="stylesheet" />
-
-    <style>
-        .dropbtn {
-
-            border: none;
-        }
-
-        .dropdown {
-            position: relative;
-            display: inline-block;
-        }
-
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #f1f1f1;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            z-index: 1;
-        }
-
-        .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-        }
-
-        .dropdown-content a:hover {
-            background-color: #ddd;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-
-        .dropdown:hover .dropbtn {
-            background-color: #3e8e41;
-        }
-    </style>
-
 </head>
 
-<body>  
+<body>
     <header>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top">
             <div class="container">
@@ -78,12 +38,61 @@
                         <li class="nav-item">
                             <a aria-current="page" class="nav-link active" href="#">Home </a>
                         </li>
+                        <div class="dropdown">
+                            <li class="nav-item">
+                                <a class="nav-link dropbtn" href="#">
+                                    Services <i class="fa fa-caret-down"></i>
+                                </a>
+                            </li>
+                            <div class="mega-menu">
+                                <div class="row">
+
+                                    <div class="col-md-4">
+                                        <h4>Category 1</h4>
+                                        <a href="#">Operations Performance Management</a>
+                                        <a href="#">Digital Worker Solutions</a>
+                                        <a href="#">Remote Operations</a>
+                                        <a href="#">Grid Analytics</a>
+                                        <a href="#">Spares and repairs</a>
+                                        <a href="#">Multibrand Maintenance</a>                                        </a>
+                                        <a href="#">Asset optimization</a>
+                                    </div>
+
+                                    <div class="col-md-4">
+                                        <h4>Category 2</h4>
+                                        <a href="#">Power Audit Services</a>
+                                        <a href="#">Oil and Gas Security Systems Audit</a>
+                                        <a href="#">Building Technology Services</a>
+                                        <a href="#">Digital Enterprise Services</a>
+                                        <a href="#">Data Analytics</a>
+                                        <a href="#">Digital Consulting & Solutions</a>
+                                        <a href="#">Mobility Services</a>
+                                    </div>
+
+                                    <div class="col-md-4">
+                                        <h4>Category 3</h4>
+                                        <a href="#">Technology Accelerator</a>
+                                        <a href="#">Building Management System Services</a>
+                                        <a href="#">Critical Power and Cooling Services</a>
+                                        <a href="#">Electrical Distribution Services</a>
+                                        <a href="#">Industrial Automation Services</a>
+                                        <a href="#">Safety Services</a>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
                         <li class="nav-item">
                             <a class="nav-link" href="#portfolio">Portfolio </a>
                         </li>
+
                         <div class="dropdown">
-                            <li class="nav-item dropbtn"><a class="nav-link" href="#products">Products <i
-                                        class="fa fa-caret-down"></i></a></li>
+                            <li class="nav-item dropbtn">
+                                <a class="nav-link" href="#products">
+                                    Products <i class="fa fa-caret-down"></i>
+                                </a>
+                            </li>
                             <div class="dropdown-content">
                                 <a href="storage/sliding/Ensmart Power EV Charging Power Module.pdf">EV Charging
                                     Power</a>
@@ -109,7 +118,7 @@
         @yield('content')
 
         @include('layouts.footer')
-        
+
     </main>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="{{ asset('vendor/aos/aos.js') }}"></script>
